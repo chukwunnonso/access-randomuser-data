4315c270dfe6a75c46d30d5dc9df7303761160dd
Chukwunnonso <cnnonso@gmail.com>
2022-11-21 07:27:00 -0800

    random-users-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FetchUsers from "./FetchUsers";
+
+const App = () => {
+  return (
+    <>
+      <FetchUsers />
+    </>
+  );
+}
+
+export default App;
---
+++ src/FetchUsers.js
@@ -0,0 +1,179 @@
+import React, {useState, useEffect} from 'react'
+import {FaPhone, FaUserCheck} from 'react-icons/fa'
+import {AiOutlineMail} from 'react-icons/ai'
+import moment from  'moment'
+const url =  `https://randomuser.me/api/`
+ 
+const FetchUsers = () => {
+  const [users, setUsers] = useState([])
+
+  const fetchUserData = async() => {
+    const resp = await fetch(url)
+    const users = await resp.json()
+    setUsers(users.results)
+    console.log(users.results);
+  }
+
+
+  useEffect(() => {
+    // const fetchUserData = async() => {
+    //   const resp = await fetch(url)
+    //   const users = await resp.json()
+    //   setUsers(users.results)
+    //   console.log(users.results);
+    // }
+
+    fetchUserData();
+  }, [])
+  return (
+    <>
+    
+      <section className='bg-gray-900 py-20 px-10'>
+        {users.map((user) => {
+            // destructuring
+            const { name:{title, first, last}, location: {
+                street: {
+                    number,
+                    name
+                },
+                city,
+                state,
+                country,
+                postcode,
+                coordinates: {
+                    latitude,
+                    longitude
+                },
+                timezone: {
+                    offset,
+                    description
+                }
+            },
+            email,
+            login:{
+                uuid,
+                username
+            },
+            dob: {
+                date,
+                age
+            },
+            phone,
+            picture: {
+                large
+            }
+        } = user
+
+            return (
+                <div key={uuid} className="bg-gray-200 px-5 py-10 rounded-lg" >
+                    <img className='block mx-auto rounded-full' src={large} alt={first}/>
+                    <div className='text-center'>
+                        <h3 className='text-2xl my-2'>{title}. {first} {last}</h3>
+                        <p>
+                            <span className='font-bold'>DOB:</span>
+                             {' '}
+                             {moment(`${date}`).format("MMMM Do YYYY")
+                             }
+                        </p>
+                        <p>AGE: {age} Years</p>
+                        <div className='underline mx-auto my-5'></div>
+                    </div>
+
+                    <div>
+                        <p className='flex-items-center mr-3 text-xl my-2'>
+                            <AiOutlineMail/> {email}</p>
+                        <p className='flex-items-center mr-3 text-xl my-2'>
+                            <FaUserCheck/> {username}</p>
+                        <p className='flex-items-center mr-3 text-xl my-2'>
+                            <FaPhone /> {phone}
+                        
+                        </p>
+                    </div>
+
+                    <div className='mt-10'>
+                        <ul>
+                            <li>{number}, {name}</li>
+                            <li className='flex items-center justify-between my-3'><span className='font-bold'>Country:</span> {country}</li>
+                            <li className='flex items-center justify-between my-3'><span  className="font-bold">State:</span> {state}</li>
+                            <li className='flex items-center justify-between my-3'><span className="font-bold">City:</span> {city}</li>
+                            <li className='flex items-center justify-between my-3'><span className="font-bold">Postal Code:</span> {postcode}</li>
+                            <li className='flex items-center justify-between my-3'><span className="font-bold">Latitude:</span> {latitude}</li>
+                            <li className='flex items-center justify-between my-3'><span className="font-bold">Longitude:</span> {longitude}</li>
+                            <li>{offset}, {description}</li>
+                        </ul>
+                    </div>
+                </div>
+            )
+        })}
+      </section>
+    </>
+  )
+}
+
+export default FetchUsers
+
+
+
+/*
+
+
+
+    <section className='bg-gray-900 py-20 px-10'>
+        {users.map((user) => {
+            // destructuring
+            const {gender, name:{title, first, last}, location: {
+                street: {
+                    number,
+                    name
+                },
+                city,
+                state,
+                country,
+                postcode,
+                coordinates: {
+                    latitude,
+                    longitude
+                },
+                timezone: {
+                    offset,
+                    description
+                }
+            },
+            email,
+            login:{
+                uuid,
+                username
+            },
+            dob: {
+                date,
+                age
+            },
+            phone,
+            picture: {
+                large
+            }
+        } = user
+
+            return (
+                <div key={uuid} className="bg-gray-200 px-5 py-10 rounded-lg" >
+                    <img className='block mx-auto' src={large} alt={first}/>
+                    <div className='text-center'>
+                        <h3 className='text-2xl my-2'>{title}. {first} {last}</h3>
+                        <p><span className='font-bold'>DOB:</span> {date}</p>
+                        <p>AGE: {age} Years</p>
+                        <div className='underline mx-auto my-5'></div>
+                    </div>
+                </div>
+            )
+        })}
+      </section>
+    </>
+  )
+}
+
+
+
+
+
+
+*/
